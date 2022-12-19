21be2fc31286cad4fcf1894eae3bf370f070c471
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-12-19 15:56:32 -0800

    task

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    hold = 0
+    for i in range(x):
+        try:
+            if my_list[i] <= 0 or my_list[i] >= 0:
+                clean = my_list[i]
+                print("{:d}".format(clean), end="")
+                hold += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return hold
