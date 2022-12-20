1c56837e8b79c6ea915d5c953e3d27d79aeb5166
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-12-20 15:52:09 -0800

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A Square Module."""
+
+
+class Square:
+    """Representation of a square."""
+    def __init__(self,  size):
+        """Constructor.
+
+        args:
+            size: The length of all sizes of a square.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"A Square Module."
+
+
+class Square:
+    """Construct of a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            Size: The length of the sides of the square.
+
+        Raise:
+            TypeError: The arguement passed is not an integer.
+            ValueError: The argument paseesd is less than zeror (0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
